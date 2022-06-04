9cf0aee963e6050229f5a470d636f5d62bb26b5e
sdperez <61090060+Sergio0x800@users.noreply.github.com>
2022-06-04 01:18:15 -0600

    modificacion de reporte evaluacion de riesgos

--- sinacig-reportes-backend/routes/reporte_evaluacion_riesgo.js
+++ sinacig-reportes-backend/routes/reporte_evaluacion_riesgo.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const CatalogosService = require('../services/catalogos.service');
+const moment = require('moment');
+
 const xl = require('excel4node');
 const estilosReportes = require('../utils/reports/estilos_reportes');
-const CatalogosService = require('../services/catalogos.service');
 const EvaluacionRiesgoReporteService = require('../services/evaluacion_riesgo_reporte.service');
 
 const catalogosServiceI = new CatalogosService();
@@ -12,110 +14,122 @@ router.post('/evaluacion_riesgo', async (req, res, next) => {
     const { unidadEjecutora, fechaInicio, fechaFin } = req.body;
 
     var wb = new xl.Workbook();
+
     var ws = wb.addWorksheet('Matriz evaluacion riesgos');
     //Obtenemos información de la unidad ejecutora por el id
     const unidadEjecutoraData = await catalogosServiceI.findUnidadEjecutoraById(
       unidadEjecutora
     );
     //Damos el ancho a las columnas especificando el numero de columna ws.column(1)
-    ws.column(1).setWidth(30);
-    ws.column(2).setWidth(30);
-    ws.column(3).setWidth(20);
-    ws.column(4).setWidth(20);
+    ws.column(1).setWidth(14);
+    ws.column(2).setWidth(14);
+    ws.column(3).setWidth(8);
+    ws.column(4).setWidth(11);
     ws.column(5).setWidth(20);
-    ws.column(6).setWidth(20);
-    ws.column(7).setWidth(20);
-    ws.column(8).setWidth(20);
-    ws.column(9).setWidth(20);
-    ws.column(10).setWidth(30);
-    ws.column(11).setWidth(30);
-    ws.column(12).setWidth(20);
+    ws.column(6).setWidth(31);
+    ws.column(7).setWidth(14);
+    ws.column(8).setWidth(11);
+    ws.column(9).setWidth(11);
+    ws.column(10).setWidth(11);
+    ws.column(11).setWidth(15);
+    ws.column(12).setWidth(33);
     ws.column(13).setWidth(20);
     ws.column(14).setWidth(35);
-    ws.column(15).setWidth(20);
 
     //Encabezado principal
-    ws.cell(2, 2, 2, 5, true)
+    const fecha_inicio = moment(fechaInicio, 'YYYY-MM-DD').format('DD/MM/YYYY');
+    const fecha_fin = moment(fechaFin, 'YYYY-MM-DD').format('DD/MM/YYYY');
+    ws.cell(2, 1, 3, 14, true)
       .string('Ministerio de Salud Pública y Asistencia Social-MSPAS-')
       .style(estilosReportes.encabezadoPrincipal);
-    ws.cell(3, 2, 3, 5, true)
+    ws.cell(4, 1, 4, 14, true)
       .string('Matriz de Evaluación de Riesgos')
+      .style(estilosReportes.encabezadoPrincipal);
+    ws.cell(5, 1, 5, 14, true)
+      .string(`Del ${fecha_inicio} al ${fecha_fin}`)
       .style(estilosReportes.encabezadoPrincipal);
 
     //Datos unidad ejecutora y periodo evaluación
-    ws.cell(7, 1).string('Unidad Ejecutora No.').style(estilosReportes.negrita);
-    ws.cell(7, 2).string(`${unidadEjecutoraData.codigo_unidad}`);
-    ws.cell(8, 1)
+    ws.cell(6, 1)
+      .string('Unidad Ejecutora No.')
+      .style(estilosReportes.infoUnidad);
+    ws.cell(6, 2).string(`${unidadEjecutoraData.codigo_unidad}`);
+    ws.cell(7, 1)
       .string('Nombre de la Unidad Ejecutora')
-      .style(estilosReportes.negrita);
-    ws.cell(8, 2, 8, 4, true).string(`${unidadEjecutoraData.nombre_unidad}`);
-    ws.cell(9, 1)
-      .string('Periodo de Evaluación')
-      .style(estilosReportes.negrita);
-    ws.cell(9, 2, 9, 3, true).string(`Del ${fechaInicio} al ${fechaFin}`);
+      .style(estilosReportes.infoUnidad);
+    ws.cell(7, 2, 7, 4, true).string(`${unidadEjecutoraData.nombre_unidad}`);
 
     //Instrucciones
-    ws.cell(12, 1, 12, 5, true).string(
-      'Instrucciones: Realice la evaluación de riesgos identificados previamente, completando la información de la matriz según lo indica'
-    );
-    ws.cell(13, 1, 13, 2, true).string('el documento SINACIG en la página 51');
+    // ws.cell(12, 1, 12, 5, true).string(
+    //   'Instrucciones: Realice la evaluación de riesgos identificados previamente, completando la información de la matriz según lo indica'
+    // );
+    // ws.cell(13, 1, 13, 2, true).string('el documento SINACIG en la página 51');
 
     //Rango registro residual y definir colores
-    ws.cell(12, 6).string('1 a 10');
-    ws.cell(13, 6).string('10.01 a 15');
-    ws.cell(14, 6).string('15.1 +');
-    ws.cell(12, 7).string('Tolerable').style(estilosReportes.backgroundVerde);
-    ws.cell(13, 7)
+    ws.cell(6, 11).string('1 a 10').style(estilosReportes.infoMedida);
+    ws.cell(7, 11).string('10.01 a 15').style(estilosReportes.infoMedida);
+    ws.cell(8, 11).string('15.1 +').style(estilosReportes.infoMedida);
+    ws.cell(6, 12, 6, 13, true)
+      .string('Tolerable')
+      .style(estilosReportes.backgroundVerde);
+    ws.cell(7, 12, 7, 13, true)
       .string('Gestionable')
       .style(estilosReportes.backgroundAmarillo);
-    ws.cell(14, 7).string('No tolerable').style(estilosReportes.backgroundRojo);
+    ws.cell(8, 12, 8, 13, true)
+      .string('No tolerable')
+      .style(estilosReportes.backgroundRojo);
 
     //Encabezado columnas
-    ws.cell(16, 7, 16, 8, true)
+    // ws.cell(16, 7, 16, 8, true)
+    //   .string('No.')
+    //   .style(estilosReportes.encabezadoTabla);
+    const filaEncabezado = 11;
+    ws.cell(10, 8, 10, 9, true)
       .string('Evaluación')
       .style(estilosReportes.encabezadoTabla);
-
-    ws.cell(17, 1)
+    ws.cell(filaEncabezado, 1)
       .string('Código Unidad Ejecutora')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 2)
-      .string('Nombre Unidad Ejecutora')
-      .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 3)
+    // ws.cell(filaEncabezado, 2)
+    //   .string('Nombre Unidad Ejecutora')
+    //   .style(estilosReportes.encabezadoTabla);
+    ws.cell(filaEncabezado, 2)
       .string('Tipo de Objetivo')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 4)
-      .string('Código de Referencia')
+    ws.cell(filaEncabezado, 3)
+      .string('Ref')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 5)
+    ws.cell(filaEncabezado, 4)
       .string('Área Evaluada')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 6)
+    ws.cell(filaEncabezado, 5)
       .string('Eventos Identificados')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 7)
+    ws.cell(filaEncabezado, 6)
       .string('Descripción Riesgo')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 8)
+    ws.cell(filaEncabezado, 7)
       .string('Probabilidad')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 9).string('Severidad').style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 10)
+    ws.cell(filaEncabezado, 8)
+      .string('Severidad')
+      .style(estilosReportes.encabezadoTabla);
+    ws.cell(filaEncabezado, 9)
       .string('Riesgo Inherente (RI)')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 11)
+    ws.cell(filaEncabezado, 10)
       .string('Valor Control Mitigador')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 12)
+    ws.cell(filaEncabezado, 11)
       .string('Riesgo Residual')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 13)
-      .string('Medida Riesgo')
-      .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 14)
+    // ws.cell(filaEncabezado, 13)
+    //   .string('Medida Riesgo')
+    //   .style(estilosReportes.encabezadoTabla);
+    ws.cell(filaEncabezado, 12)
       .string('Control Interno para Mitigar')
       .style(estilosReportes.encabezadoTabla);
-    ws.cell(17, 15)
+    ws.cell(filaEncabezado, 13)
       .string('Observaciones')
       .style(estilosReportes.encabezadoTabla);
 
@@ -127,27 +141,40 @@ router.post('/evaluacion_riesgo', async (req, res, next) => {
     );
 
     //indicamos el número de fila donde se comenzará a escribir la información del reporte
-    let rowIndex = 18;
+    let rowIndex = 12;
     resultado[0].forEach((item) => {
       let columnIndex = 1;
       Object.keys(item).forEach((colName) => {
-        if (item.riesgo_residual >= 0 && item.riesgo_residual <= 10) {
-          ws.cell(rowIndex, 12)
-            .string(item.riesgo_residual.toString())
-            .style(estilosReportes.backgroundVerde);
-        } else if (item.riesgo_residual > 10 && item.riesgo_residual <= 15) {
-          ws.cell(rowIndex, 12)
-            .string(item.riesgo_residual.toString())
-            .style(estilosReportes.backgroundAmarillo);
-        } else if (item.riesgo_residual > 15) {
-          ws.cell(rowIndex, 12)
-            .string(item.riesgo_residual.toString())
-            .style(estilosReportes.backgroundRojo);
+        if (colName === 'Riesgo Residual') {
+          if (item[colName] >= 0 && item[colName] <= 10) {
+            ws.cell(rowIndex, columnIndex++)
+              .string(item[colName].toString())
+              .style(estilosReportes.backgroundVerde);
+          } else if (item[colName] >= 10.01 && item[colName] <= 15) {
+            ws.cell(rowIndex, columnIndex++)
+              .string(item[colName].toString())
+              .style(estilosReportes.backgroundAmarillo);
+          } else if (item[colName] >= 15.01) {
+            ws.cell(rowIndex, columnIndex++)
+              .string(item[colName].toString())
+              .style(estilosReportes.backgroundRojo);
+          } else {
+            ws.cell(rowIndex, columnIndex++)
+              .string(item[colName].toString())
+              .style(estilosReportes.infoTabla);
+          }
+        } else {
+          ws.cell(rowIndex, columnIndex++)
+            .string(item[colName].toString())
+            .style(estilosReportes.infoTabla);
         }
-        ws.cell(rowIndex, columnIndex++).string(item[colName].toString());
       });
       rowIndex++;
     });
+
+    // resultado[0].forEach((riesgo) => {
+    //   console.log(riesgo);
+    // });
 
     //Pie de página
     ws.cell(rowIndex + 2, 1)
--- sinacig-reportes-backend/utils/reports/estilos_reportes.js
+++ sinacig-reportes-backend/utils/reports/estilos_reportes.js
@@ -2,85 +2,238 @@ const xl = require('excel4node');
 var wb = new xl.Workbook();
 
 const estilosReportes = {
-    encabezadoPrincipal: wb.createStyle({
-        font: {
-            bold: true,
-            size: 15
-        },
-        alignment: {
-            horizontal: 'center',
-            vertical: 'center'
-        },
-    }),
-    encabezadoTabla: wb.createStyle({
-        font: {
-            bold: true,
-            size: 12
-        },
-        alignment: {
-            horizontal: 'center',
-            vertical: 'center'
-        },
-    }),
-    negrita: wb.createStyle({
-        font: {
-            bold: true,
-            size: 12
-        },
-    }),
-    backgroundAmarillo: wb.createStyle({
-        fill: {
-            type: 'pattern',
-            patternType: 'solid',
-            bgColor: '#FFFF00',
-            fgColor: '#FFFF00',
-          },
-        alignment: { 
-            shrinkToFit: true, 
-            wrapText: true
-        }
-    }),
-    backgroundRojo: wb.createStyle({
-        fill: {
-            type: 'pattern',
-            patternType: 'solid',
-            bgColor: '#D11003',
-            fgColor: '#D11003',
-          },
-          alignment: { 
-            shrinkToFit: true, 
-            wrapText: true
-        }
-    }),
-    backgroundVerde: wb.createStyle({
-        fill: {
-            type: 'pattern',
-            patternType: 'solid',
-            bgColor: '#14BE00',
-            fgColor: '#14BE00',
-          },
-        alignment: { 
-            shrinkToFit: true, 
-            wrapText: true
-        }
-    }),
-    backgroundGris: wb.createStyle({
-        fill: {
-            type: 'pattern',
-            patternType: 'solid',
-            bgColor: '#D6F3BC',
-            fgColor: '#D6F3BC'
-        }
-    }),
-    backgroundRosado: wb.createStyle({
-        fill: {
-            type: 'pattern',
-            patternType: 'solid',
-            bgColor: '#F79B87',
-            fgColor: '#F79B87'
-        }
-    })
+  encabezadoPrincipal: wb.createStyle({
+    font: {
+      bold: true,
+      size: 11,
+      name: 'Arial',
+    },
+    alignment: {
+      horizontal: 'center',
+      vertical: 'center',
+    },
+  }),
 
+  encabezadoTabla: wb.createStyle({
+    border: {
+      left: {
+        style: 'thin',
+        color: 'black',
+      },
+      right: {
+        style: 'thin',
+        color: 'black',
+      },
+      top: {
+        style: 'thin',
+        color: 'black',
+      },
+      bottom: {
+        style: 'thin',
+        color: 'black',
+      },
+      outline: false,
+    },
+    font: {
+      bold: true,
+      size: 11,
+      name: 'Arial',
+    },
+    alignment: {
+      shrinkToFit: true,
+      wrapText: true,
+      horizontal: 'center',
+      vertical: 'center',
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      bgColor: '#D9D9D9',
+      fgColor: '#D9D9D9',
+    },
+  }),
 
-}
-module.exports = estilosReportes
+  infoTabla: wb.createStyle({
+    border: {
+      left: {
+        style: 'thin',
+        color: 'black',
+      },
+      right: {
+        style: 'thin',
+        color: 'black',
+      },
+      top: {
+        style: 'thin',
+        color: 'black',
+      },
+      bottom: {
+        style: 'thin',
+        color: 'black',
+      },
+      outline: false,
+    },
+    font: {
+      size: 11,
+      name: 'Arial',
+    },
+    alignment: {
+      shrinkToFit: true,
+      wrapText: true,
+      horizontal: 'center',
+      vertical: 'center',
+    },
+  }),
+
+  negrita: wb.createStyle({
+    font: {
+      bold: true,
+      size: 12,
+    },
+  }),
+  infoUnidad: wb.createStyle({
+    font: {
+      name: 'Arial',
+      size: 11,
+      color: '#000000',
+    },
+  }),
+  infoMedida: wb.createStyle({
+    font: {
+      name: 'Arial',
+      size: 11,
+      color: '#000000',
+    },
+  }),
+  backgroundAmarillo: wb.createStyle({
+    border: {
+      left: {
+        style: 'thin',
+        color: 'black',
+      },
+      right: {
+        style: 'thin',
+        color: 'black',
+      },
+      top: {
+        style: 'thin',
+        color: 'black',
+      },
+      bottom: {
+        style: 'thin',
+        color: 'black',
+      },
+      outline: false,
+    },
+    font: {
+      name: 'Arial',
+      size: 11,
+      color: '#000000',
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      bgColor: '#FFFF00',
+      fgColor: '#FFFF00',
+    },
+    alignment: {
+      shrinkToFit: true,
+      wrapText: true,
+      horizontal: 'center',
+      vertical: 'center',
+    },
+  }),
+  backgroundRojo: wb.createStyle({
+    border: {
+      left: {
+        style: 'thin',
+        color: 'black',
+      },
+      right: {
+        style: 'thin',
+        color: 'black',
+      },
+      top: {
+        style: 'thin',
+        color: 'black',
+      },
+      bottom: {
+        style: 'thin',
+        color: 'black',
+      },
+      outline: false,
+    },
+    font: {
+      name: 'Arial',
+      size: 11,
+      color: '#000000',
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      bgColor: '#D11003',
+      fgColor: '#D11003',
+    },
+    alignment: {
+      shrinkToFit: true,
+      wrapText: true,
+      horizontal: 'center',
+      vertical: 'center',
+    },
+  }),
+  backgroundVerde: wb.createStyle({
+    border: {
+      left: {
+        style: 'thin',
+        color: 'black',
+      },
+      right: {
+        style: 'thin',
+        color: 'black',
+      },
+      top: {
+        style: 'thin',
+        color: 'black',
+      },
+      bottom: {
+        style: 'thin',
+        color: 'black',
+      },
+      outline: false,
+    },
+    font: {
+      name: 'Arial',
+      size: 11,
+      color: '#000000',
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      bgColor: '#00B050',
+      fgColor: '#00B050',
+    },
+    alignment: {
+      shrinkToFit: true,
+      wrapText: true,
+      horizontal: 'center',
+      vertical: 'center',
+    },
+  }),
+  backgroundGris: wb.createStyle({
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      bgColor: '#D6F3BC',
+      fgColor: '#D6F3BC',
+    },
+  }),
+  backgroundRosado: wb.createStyle({
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      bgColor: '#F79B87',
+      fgColor: '#F79B87',
+    },
+  }),
+};
+module.exports = estilosReportes;
